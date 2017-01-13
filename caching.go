d6ddb81b9d1d287bcfb77cffed1da4653415e2f0
Michal Bock <SpeedyCoder@users.noreply.github.com>
2017-01-13 17:17:14 +0000

    Added removing of expired items from the cache.

--- caching.go
+++ caching.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"log"
 	"time"
+	"github.com/orcaman/concurrent-map"
 )
 
 /** TODO:
@@ -32,30 +33,54 @@ type cacheValue struct {
 	time time.Time
 }
 
+// Method that removes expired items from the cache
+func cacheCleaner(cache cmap.ConcurrentMap, shutDown <-chan bool, sleepTime time.Duration) {
+	done := false
+	for !done {
+		time.Sleep(sleepTime)
+		select {
+			case <-shutDown: { done = true; continue }
+			default: {}
+		}
+		for _, key := range cache.Keys() {
+			elem, _ := cache.Get(key)
+			cv := elem.(cacheValue)
+			if (cv.time.Before(time.Now())) {
+				cache.Remove(key)
+				log.Println("Item removed")
+			}
+		}
+	}
+}
+
 func CacheComponent(c Component, expiration time.Duration) Component {
 	return func (in <-chan Value, out chan<- Value) {
 		toWorker := make(ValueChan)
 		fromWorker := make(ValueChan)
 		go c(toWorker, fromWorker)
 
-		cache := make(map[string]cacheValue)
+		cleanerShutDown := make(chan bool, 1)
+		cache := cmap.New()
+		go cacheCleaner(cache, cleanerShutDown, expiration)
+
 		var computeAndAdd = func (key string, val Value, now time.Time) {
 			toWorker <- val
             res := <- fromWorker
-            cache[key] = cacheValue{res.Result, now.Add(expiration)}
+            cache.Set(key, cacheValue{res.Result, now.Add(expiration)})
             out <- res
 		}
 
 	    for val := range in {
 	    	if (stringInSlice(val.Request.Method, CachableMethods)) {
 	    		key := requestToString(val.Request)
-		        elem, in := cache[key]
+		        elem, in := cache.Get(key)
 		        now := time.Now()
 
 		        if (in) {
-		            if (elem.time.After(now)) {
+		        	cv := elem.(cacheValue)
+		            if (cv.time.After(now)) {
 		            	log.Println("In cache\n")
-		            	val.Result = elem.value
+		            	val.Result = cv.value
 		            	out <- val
 		            } else {
 		            	log.Println("Cache expired\n")
@@ -73,6 +98,7 @@ func CacheComponent(c Component, expiration time.Duration) Component {
 	    	}
 	        
 	    }
+	    cleanerShutDown <- true
 	    close(toWorker) // To shut down the component c
 	    close(out)	
 	}
