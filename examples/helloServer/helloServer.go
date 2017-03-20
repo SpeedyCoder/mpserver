08d93390d3eadf3a01bc880b7d1d8c955c599687
Michal Bock <SpeedyCoder@users.noreply.github.com>
2017-03-20 12:47:13 +0000

    Made code snippets into figures and added figure for caching.

--- examples/helloServer/helloServer.go
+++ examples/helloServer/helloServer.go
@@ -7,16 +7,15 @@ import(
 )
 
 func main() {
-    in := make(mpserver.ValueChan)
-    out := make(mpserver.ValueChan)
-    errChan := make(mpserver.ValueChan)
-
-    go mpserver.ConstantComponent("Hello world!")(in, out)
-    go mpserver.StringWriter(out, errChan)
-    go mpserver.ErrorWriter(errChan)
+    in := mpserver.GetChan()
+    out := mpserver.GetChan()
 
     mux := http.NewServeMux()
     mpserver.Listen(mux, "/hello", in)
+
+    go mpserver.ConstantComponent("Hello world!")(in, out)
+    go mpserver.StringWriter(out, nil)
+    
     log.Println("Listening on port 3000...")
     http.ListenAndServe(":3000", mux)
 }
