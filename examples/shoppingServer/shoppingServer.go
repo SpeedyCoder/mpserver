72a5df8cf5f594a5a402ffb1ccdf3e56173e3077
Michal Bock <SpeedyCoder@users.noreply.github.com>
2017-05-09 17:11:17 +0100

    Refactored shopping example.

--- examples/shoppingServer/shoppingServer.go
+++ examples/shoppingServer/shoppingServer.go
@@ -30,8 +30,8 @@ type RemoveAction struct {
 type BuyAction struct {}
 
 // Definitions of performAction method
-func (a AddAction) performAction(s ShoppingCart) 
-                                (mpserver.State, error) {
+func (a AddAction) performAction(
+    s ShoppingCart) (mpserver.State, error) {
     if (s.items == nil) {
         s.items = make(map[string]int)
     }
@@ -43,8 +43,8 @@ func (a AddAction) performAction(s ShoppingCart)
     return s, nil
 }
 
-func (a RemoveAction) performAction(s ShoppingCart) 
-                                (mpserver.State, error) {
+func (a RemoveAction) performAction(
+    s ShoppingCart) (mpserver.State, error) {
     if (s.items == nil) {
         return nil, errors.New("Shopping cart is empty.")
     }
@@ -63,8 +63,8 @@ func (a RemoveAction) performAction(s ShoppingCart)
     return s, nil
 }
 
-func (a BuyAction) performAction(s ShoppingCart) 
-                                (mpserver.State, error) {
+func (a BuyAction) performAction(
+    s ShoppingCart) (mpserver.State, error) {
     // In real application this is where the payment and writing
     // to the database would happen
     s.bought = true
@@ -72,8 +72,8 @@ func (a BuyAction) performAction(s ShoppingCart)
 }
 
 // Definition of methods of the State interface
-func (s ShoppingCart) Next(val mpserver.Value) 
-                                (mpserver.State, error) {
+func (s ShoppingCart) Next(
+    val mpserver.Value) (mpserver.State, error) {
     a, ok := val.GetResult().(Action)
     if (!ok) {
         return nil, errors.New("Action not provided")
@@ -91,18 +91,16 @@ func (s ShoppingCart) Terminal() bool {
 }
 
 // Definition of Action makers
-func addActionMakerFunc(val mpserver.Value) mpserver.Value {
+func addActionMakerFunc(val mpserver.Value) {
     q := val.GetRequest().URL.Query()
     items, ok := q["item"]
     if (ok && len(items) > 0) {
         items = strings.Split(items[0], ",")
     }
     val.SetResult(AddAction{items})
-    
-    return val
 }
 
-func removeActionMakerFunc(val mpserver.Value) mpserver.Value {
+func rmvActionMakerFunc(val mpserver.Value) {
     q := val.GetRequest().URL.Query()
     items, ok := q["item"]
     if (ok && len(items) > 0) {
@@ -110,53 +108,79 @@ func removeActionMakerFunc(val mpserver.Value) mpserver.Value {
     } else {
         val.SetResult(
             errors.New("removeActionMaker: Item not provided."))
-    }
-    
-    return val
+    } 
 }
 
-func buyActionMakerFunc(val mpserver.Value) mpserver.Value {
+func buyActionMakerFunc(val mpserver.Value) {
     val.SetResult(BuyAction{})
-    return val
 }
 
 var addActionMaker = mpserver.MakeComponent(addActionMakerFunc)
-var removeActionMaker = mpserver.MakeComponent(
-                                        removeActionMakerFunc)
+var rmvActionMaker = mpserver.MakeComponent(
+                                        rmvActionMakerFunc)
 var buyActionMaker = mpserver.MakeComponent(buyActionMakerFunc)
 
+const SessionExpiration = time.Minute*5
+const AddTimeout = time.Second
+const RemoveTimeout = time.Minute*5
+var InitialState = ShoppingCart{nil, false}
+
+func actionWriter(actionMaker mpserver.Component, 
+                  storage mpserver.Storage) mpserver.Writer {
+    return func (in <-chan mpserver.Value) {
+        // Define session component
+        seshComp := mpserver.SessionManagementComponent(
+                storage, InitialState, SessionExpiration)
+        // Wrap it in an Error Passer
+        seshComp = mpserver.ErrorPasser(seshComp)
+
+        // Create channels
+        toSeshManager := mpserver.GetChan()
+        toSplitter := mpserver.GetChan()
+        toWriter := mpserver.GetChan()
+        toErrWriter := mpserver.GetChan()
+
+        // Start the components and writers
+        go actionMaker(in, toSeshManager)
+        go seshComp(toSeshManager, toSplitter)
+        go mpserver.ErrorSplitter(
+            toSplitter, toWriter, toErrWriter)
+        go mpserver.JsonWriter(toWriter)
+        mpserver.ErrorWriter(toErrWriter)
+    }
+}
+
 func main() {
+    // Create the storage
+    storage := mpserver.NewMemStorage()
+    // Create writers
+    addActionWriter := actionWriter(addActionMaker, storage)
+    rmvActionWriter := actionWriter(rmvActionMaker, storage)
+    buyActionWriter := actionWriter(buyActionMaker, storage)
+
+    // Wrap them in Load Balancers
+    addActionWriter = mpserver.DynamicLoadBalancerWriter(
+        addActionWriter, 100, AddTimeout, RemoveTimeout)
+    rmvActionWriter = mpserver.DynamicLoadBalancerWriter(
+        rmvActionWriter, 20, AddTimeout, RemoveTimeout)
+    buyActionWriter = mpserver.DynamicLoadBalancerWriter(
+        buyActionWriter, 40, AddTimeout, RemoveTimeout)
+
     // Make channels
     toAddActionMaker := make(mpserver.ValueChan)
-    toRemoveActionMaker := make(mpserver.ValueChan)
+    toRmvActionMaker := make(mpserver.ValueChan)
     toBuyActionMaker := mpserver.GetChan()
-    in := mpserver.GetChan()
-    out := mpserver.GetChan()
-    toWriter := mpserver.GetChan()
-    errChan := mpserver.GetChan()
 
-    // Start action makers
-    go addActionMaker(toAddActionMaker, in)
-    go removeActionMaker(toRemoveActionMaker, in)
-    go buyActionMaker(toBuyActionMaker, in)
-
-    // Start the session manager
-    initial := ShoppingCart{nil, false}
-    store := mpserver.NewMemStore()
-    sComp := mpserver.SessionManagementComponent(
-                store, initial, time.Second*60*5, true)
-    sComp = mpserver.ErrorPasser(sComp)
-    go sComp(in, out)
-
-    // Start Error Splitter and Writers
-    go mpserver.ErrorSplitter(out, toWriter, errChan)
-    go mpserver.JsonWriter(toWriter)
-    go mpserver.ErrorWriter(errChan)
+    // Start the load balanced writers
+    go addActionWriter(toAddActionMaker)
+    go rmvActionWriter(toRmvActionMaker)
+    go buyActionWriter(toBuyActionMaker)
+    go mpserver.StorageCleaner(storage, nil, SessionExpiration)
 
     // Start the server
     mux := http.NewServeMux()
     mpserver.Listen(mux, "/add", toAddActionMaker)
-    mpserver.Listen(mux, "/remove", toRemoveActionMaker)
+    mpserver.Listen(mux, "/remove", toRmvActionMaker)
     mpserver.Listen(mux, "/buy", toBuyActionMaker)
     log.Println("Listening on port 3000...")
     http.ListenAndServe(":3000", mux)
