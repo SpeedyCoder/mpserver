37fb4fed96b4824b2cc3b6c4c3a6c4d0ab30aa4e
Michal Bock <SpeedyCoder@users.noreply.github.com>
2017-05-13 16:38:43 +0100

    Refactored storage and SessionManager.

--- SessionManagers.go
+++ SessionManagers.go
@@ -1,7 +1,6 @@
 package mpserver
 
 import(
-    "log"
     "errors"
     "time"
     "crypto/rand"
@@ -35,23 +34,41 @@ func GenerateRandomString(s int) (string, error) {
     return base64.URLEncoding.EncodeToString(b), err
 }
 
+// State is a type that represents the current state of a session
+// for a single user.
 type State interface {
+    // Next returns the next State of the session for the given 
+    // user or an error if next State cannot be generated.
     Next(val Value) (State, error)
+
+    // Terminal returns a boolean that indicates whether this 
+    // State is a terminal state.
     Terminal() bool
+
+    // Result returns a result that should be returned to the 
+    // user. This should only be called after the state was 
+    // updated.
     Result() interface{}
 }
 
-func startNewSession(val Value, initial State, seshExp time.Duration, storage Storage, out chan<- Value) {
+// startNewSession is a helper function that starts a new session
+// for the given value. It generates a new session id and the 
+// current state of the session from the initial state. The 
+// mapping from the generated state is stored in the storage 
+// object and the result of the call to the Result function on
+// the current state is stored in the result field of the value,
+// before it is sent to the out channel.
+func startNewSession(val Value, initial State, 
+    seshExp time.Duration, storage Storage, out chan<- Value) {
     id, err := GenerateRandomString(32)
     if (err != nil) {
-        // if the random generator fails
-        log.Println(err)
+        // The random generator failed.
         val.SetResult(errors.New("Session-Id generation failed"))
         out <- val
         return
     }
-    log.Println("Id generated: " + id)
 
+    // Generate the current state.
     state, err := initial.Next(val)
     if (err != nil) {
         val.SetResult(err)
@@ -59,48 +76,52 @@ func startNewSession(val Value, initial State, seshExp time.Duration, storage St
         return
     }
 
+    // Store the mapping from the id to the current state.
     storage.Set(id, StorageValue{state, time.Now().Add(seshExp)})
     val.SetResult(state.Result())
     val.SetHeader("Session-Id", id)
     out <- val
 }
 
-func SessionManagementComponent(storage Storage, initial State, seshExp time.Duration) Component {
+// SessionManager returns a component that performs session
+// management.
+func SessionManager(storage Storage, initial State, 
+                    seshExp time.Duration) Component {
     return func (in <-chan Value, out chan<- Value) {
         for val := range in {
-            // log.Println(val.Request.Header)
             id := val.GetRequest().Header.Get("Session-Id")
-            log.Println(id)
             if (id == ""){
-                log.Println("No Session-Id")
+                // No Session-Id was provided.
                 startNewSession(val, initial, seshExp, storage, out)
                 continue
             }
             storageValue, in := storage.Get(id)
             if (!in){
-                log.Println("Session-Id not in storage")
-                // Invalid or expiredID
+                // Session-Id is not in the storage, hence it is 
+                // either invalid or it expired and was removed 
+                // from the storage.
                 startNewSession(val, initial, seshExp, storage, out)
                 continue
             }
 
             now := time.Now()
             if (storageValue.Time.After(now)) {
+                // Session hasn't expired yet.
                 state, _ := storageValue.Value.(State)
                 next, err := state.Next(val)
                 if (err != nil) {
-                    log.Println("Error while generating next state")
+                    // Can't generate the next state.
                     val.SetResult(err)
                     out <- val
                     continue
                 }
                 if (next.Terminal()) {
-                    log.Println("Terminal state")
-                    // If next is a terminal state, then 
-                    // the session terminates
+                    // Next state is a terminal state, so the 
+                    // session terminates.
                     storage.Remove(id)
                 } else {
-                    log.Println("Updated state")
+                    // Update the state in the storage, as 
+                    // current state is not terminal.
                     storageValue.Time = time.Now().Add(seshExp)
                     storageValue.Value = next
                     storage.Set(id, storageValue)
@@ -110,8 +131,8 @@ func SessionManagementComponent(storage Storage, initial State, seshExp time.Dur
                 val.SetResult(next.Result())
                 out <- val
             } else {
-                log.Println("Session expired")
-                // Session expired
+                // Session expired, so try to start a new session
+                // for this user.
                 storage.Remove(id)
                 startNewSession(val, initial, seshExp, storage, out)
             }
@@ -119,5 +140,3 @@ func SessionManagementComponent(storage Storage, initial State, seshExp time.Dur
         close(out)
     }
 }
-
-
---
+++ Storage.go
@@ -0,0 +1,106 @@
+package mpserver
+
+import (
+	"github.com/orcaman/concurrent-map"
+	"time"
+)
+
+// StorageValue is a type a values that are stored in the Storage
+// object.
+type StorageValue struct {
+	Value interface{} // The stored value itself.
+	Time time.Time 	  // Expiration time for this value.
+}
+
+// Storage represents a thread safe type that represents a 
+// mapping from string keys to StorageValues.
+type Storage interface {
+	// Get returns the storage value for the key and a boolean 
+	// indicating whether the key is stored in the map.
+	Get(key string) (StorageValue, bool)
+
+	// Set stores the mapping from the given key to the given 
+	// value.
+	Set(key string, value StorageValue)
+
+	// Remove removes the key-value pair from the map for the 
+	// provided key.
+	Remove(key string)
+
+	// CompareAndRemove checks if the value stored in the map for
+	// the given key is deep equal to the provided value. If that
+	// is the case it removes the key-value pair from the map.
+	// The boolean returned indicates whether the key-value pair  
+	// was removed.
+	CompareAndRemove(key string, value StorageValue) bool
+	
+	// Keys returns a slice that contains all keys that are 
+	// stored in the mapping.
+	Keys() []string
+}
+
+// memStorage is a type that implements the Storage interface.
+// It uses sharded map internally to store the mapping.
+type memStorage struct {
+	cMap *cmap.ConcurrentMap
+}
+
+func (ms memStorage) Get(key string) (StorageValue, bool) {
+	elem, in := ms.cMap.Get(key)
+	if (in) {
+		storageValue := elem.(StorageValue)
+		return storageValue, true
+	}
+
+	return StorageValue{}, false
+}
+
+func (ms memStorage) Set(key string, value StorageValue) {
+	ms.cMap.Set(key, value)
+}
+
+func (ms memStorage) Remove(key string) {
+	ms.cMap.Remove(key)
+}
+
+func (ms memStorage) CompareAndRemove(key string, 
+									  value StorageValue) bool {
+	return ms.cMap.CompareAndRemove(key, value)
+}
+
+func (ms memStorage) Keys() []string {
+	return ms.cMap.Keys()
+}
+
+// NewMemStorage returns a Storage object that stores the values
+// in memory. 
+func NewMemStorage() Storage {
+	cMap := cmap.New()
+	return memStorage{&cMap}
+}
+
+
+// StorageCleaner is a function that repeatedly removes expired 
+// values from the provided Storage object. After it goes through
+// all keys in the mapping it sleeps for time specified by the
+// sleepTime argument. The function can be shutdown, by sending a
+// message on the shutdown channel.
+func StorageCleaner(storage Storage, shutDown <-chan bool, 
+					sleepTime time.Duration) {
+	done := false
+	for !done {
+		time.Sleep(sleepTime)
+		select {
+			case <-shutDown: { done = true; continue }
+			default: {}
+		}
+		for _, key := range storage.Keys() {
+			storageValue, _ := storage.Get(key)
+			if (storageValue.Time.Before(time.Now())) {
+				// Cache can be updated at this point, so the following
+				// Remove can remove an entry, which hasn't expired yet
+				storage.CompareAndRemove(key, storageValue)
+			}
+		}
+	}
+}
